e08245742f3d176822bb9c7047ceee31f21f5a38
Milos Stevanovic <mstevanovic2@gvcgroup.com>
2020-08-23 16:14:35 +0200

    Rename Open In ConEmu to Open In IceTerm
    It is easier to find commands when they are of the same name as the plugin itself

---
+++ src/java/org/iceterm/action/OpenInCurrentTab.java
@@ -0,0 +1,37 @@
+package org.iceterm.action;
+
+import com.intellij.ide.actions.RevealFileAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.project.DumbAwareAction;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.iceterm.IceTermView;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class OpenInCurrentTab extends DumbAwareAction {
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        Project project = getEventProject(e);
+        e.getPresentation().setEnabledAndVisible(project != null && getSelectedFile(e) != null);
+    }
+
+    @Nullable
+    private static VirtualFile getSelectedFile(@NotNull AnActionEvent e) {
+        return RevealFileAction.findLocalFile(e.getData(CommonDataKeys.VIRTUAL_FILE));
+    }
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        Project project = getEventProject(e);
+        VirtualFile selectedFile = getSelectedFile(e);
+        if (project == null || selectedFile == null) {
+            return;
+        }
+        IceTermView iceTermView = IceTermView.getInstance(project);
+        if(iceTermView != null) {
+            iceTermView.openTerminalIn(selectedFile, false);
+        }
+    }
+}
